39577e79fc7077f6ccd6c146e32fa7c21973ae2f
Victor <victordesousaa@gmail.com>
2025-06-16 23:03:41 -0300

    feat: first arrays solution

--- arrays/src/application/Main.java
+++ arrays/src/application/Main.java
@@ -6,9 +6,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Quantos números você irá digitar?");
+		System.out.print("How many numbers are you typing?");
 		int n = sc.nextInt();
-		
+		int[] array = new int[n];
+		for (int i=0; i<n; i++) {
+			System.out.print("Type a number: ");
+			int number = sc.nextInt();
+			array[i] = number;
+		};
+		System.out.println("Negative numbers: ");
+		for (int i=0; i<n; i++) {
+
+			if (array[i] < 0) {
+				System.out.println(array[i]);
+			}
+		};
+		sc.close();
 		
 	}
 }
