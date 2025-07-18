c3422441bf0df62fb0b333236c058072200fdcbc
VictorAlencar00 <victordesousaa@gmail.com>
2025-07-17 23:56:29 -0300

    fix: unused throw removal

--- generics-set-map/map-count/src/application/Main.java
+++ generics-set-map/map-count/src/application/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         Map<String, Integer> candidates = new LinkedHashMap<>();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter file path: ");
